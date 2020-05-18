bd9877ce53e3df719ba7cd43c421bf8b1c1446c8
Carla Joseph <carlajseph@gmail.com>
2020-05-18 09:10:14 -0400

    I have most of the codes but it's a matter of putting them in the right place

--- Program.cs
+++ Program.cs
@@ -2,26 +2,62 @@
 using System.Collections.Generic;
 
 namespace blackjack
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var suits = new List<string>() { "Spades", "Clubs", "Diamonds", "Hearts" };
-            var ranks = new List<string>() {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King};
-            var deck = new List<string> ();
-            var shuffled = new List<string>();
 
-            for (var suitIndex = 0; suitIndex < suits.Count; suitIndex++)
-            {
-                for (var rankIndex = 0; rankIndex < ranks.Count; rankIndex++)
-                {
-                    deck.Add($"{suits[suitIndex]} of {ranks[rankIndex]}");
-                }
-            }
-            for (var deckIndex = 0; deckIndex < deck.Count; deckIndex++)
-            {
-                Console.WriteLine(deck[deckIndex]);
-            }
-        }
-    } 
+    static string GetCardValue(int cards)
+{
+    string Card;
+    switch (cards)
+    {
+        case 1:
+            Card = "Two"; playerTotal += 2;
+            break;
+        case 2:
+            Card = "Three"; playerTotal += 3;
+            break;
+        case 3:
+            Card = "Four"; playerTotal += 4;
+            break;
+        case 4:
+            Card = "Five"; playerTotal += 5;
+            break;
+        case 5:
+            Card = "Six"; playerTotal += 6;
+            break;
+        case 6:
+            Card = "Seven"; playerTotal += 7;
+            break;
+        case 7:
+            Card = "Eight"; playerTotal += 8;
+            break;
+        case 8:
+            Card = "Nine"; playerTotal += 9;
+            break;
+        case 9:
+            Card = "Ten"; playerTotal += 10;
+            break;
+        case 10:
+            Card = "Jack"; playerTotal += 10;
+            break;
+        case 11:
+            Card = "Queen"; playerTotal += 10;
+            break;
+        case 12:
+            Card = "King"; playerTotal += 10;
+            break;
+        case 13:
+            Card = "Ace"; playerTotal += 11;
+            break;
+    }
+    return Card;
+}
+class Card
+{
+    public string Face { get; set; }
+    public string Suit { get; set; }
+}
+static string DealCard()
+{
+    int cards = cardRandomizer.Next(1, 14);
+    string Card = GetCardValue(cards);
+    return Card;
+}
